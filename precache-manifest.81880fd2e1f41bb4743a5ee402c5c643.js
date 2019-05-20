334190172a6b820e77b55a52104e67d7aafcebdb
Almog <almogbh.l@gmail.com>
2019-05-20 18:01:47 +0300

    Updates

---
+++ precache-manifest.81880fd2e1f41bb4743a5ee402c5c643.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "459903c13e6b09afbe71",
+    "url": "/GitHub-Repositories/static/js/runtime~main.f790e0bd.js"
+  },
+  {
+    "revision": "d99240a7e7f48de88e44",
+    "url": "/GitHub-Repositories/static/js/main.25b77abd.chunk.js"
+  },
+  {
+    "revision": "b7e4cb4df8f6aba04696",
+    "url": "/GitHub-Repositories/static/js/2.c47c42bc.chunk.js"
+  },
+  {
+    "revision": "8efc06f0e9cca034a238791a0c6fbe2e",
+    "url": "/GitHub-Repositories/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/almogbhl/GitHub-Repositories/precache-manifest.fa7eb28dc24b912dfdf98ddcb5747d58.js"
+  "/GitHub-Repositories/precache-manifest.81880fd2e1f41bb4743a5ee402c5c643.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/almogbhl/GitHub-Repositories/index.html", {
+workbox.routing.registerNavigationRoute("/GitHub-Repositories/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
